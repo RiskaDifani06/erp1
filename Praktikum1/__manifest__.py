9b1238ff2d21b91ab4db6168c895163a57d03bf3
RiskaDifani06 <riskadifani66@gmail.com>
2023-02-22 09:12:07 +0700

    p

---
+++ Praktikum1/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name"       : "New Addons",
+    "version"    : "1.0",
+    "author"     : "Riska Indah Difani",
+    "website"    : "https://www.detik.com/",
+    "category"   : "New Module",
+    "summary"    : "Ini adalah addons baru",
+    "description": """
+            Latihan membuat addons baru di odoo 1
+            """,
+    "depends" :[
+        "base",
+    ],
+    "data" :[
+
+
+    ],
+    "demo"      : [],
+    "test"      : [],
+    "image"     : [],
+    "qweb"      : [],
+    "css"       : [],
+   "application": True,
+   "insallable" : True,
+   "auto_install": False,
+
+}
